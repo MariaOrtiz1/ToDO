a5910c1b04490f47110e4455a7b5d6465f99f4b6
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-06-30 00:49:37 -0700

    first test passed, very simple but baby steps!

--- data/todos.js
+++ data/todos.js
@@ -3,18 +3,18 @@ module.exports = [
     id: 1,
     todo: 'task1',
     completed: false,
-    user_id: 1
+    owner_id: 1
   },
   {
     id: 2,
     todo: 'task2',
     completed: false,
-    user_id: 1
+    owner_id: 1
   },
   {
     id: 3,
     todo: 'task3',
     completed: false,
-    user_id: 1
+    owner_id: 1
   }
 ];
